208b2464a5c3c13b24c086b36ac6c344927f05e0
Alex Shamrai <gibsonshamray@gmail.com>
2024-06-08 14:15:29 +0300

    Add UserTest

--- ui-tests/src/main/java/com/practicesoftwaretesting/pages/LoginPage.java
+++ ui-tests/src/main/java/com/practicesoftwaretesting/pages/LoginPage.java
@@ -1,10 +1,16 @@
 package com.practicesoftwaretesting.pages;
 
+import org.openqa.selenium.By;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.practicesoftwaretesting.utils.SelectorUtils.byDataTest;
 
 public class LoginPage {
+
+    private static final By EMAIL_INPUT = byDataTest("email");
+    private static final By PASSWORD_INPUT = byDataTest("password");
+    private static final By LOGIN_BUTTON = byDataTest("login-submit");
 
     public LoginPage isLoaded() {
         $("h3").shouldHave(text("Login"));
@@ -13,5 +19,11 @@ public class LoginPage {
 
     public void clickRegisterYourAccountLink() {
         $(byDataTest("register-link")).click();
+    }
+
+    public void login(String email, String password) {
+        $(EMAIL_INPUT).setValue(email);
+        $(PASSWORD_INPUT).setValue(password);
+        $(LOGIN_BUTTON).click();
     }
 }
--- ui-tests/src/test/java/com/practicesoftwaretesting/UserTest.java
+++ ui-tests/src/test/java/com/practicesoftwaretesting/UserTest.java
@@ -1,9 +1,7 @@
 package com.practicesoftwaretesting;
 
-import com.practicesoftwaretesting.pages.Header;
-import com.practicesoftwaretesting.pages.HomePage;
-import com.practicesoftwaretesting.pages.LoginPage;
-import com.practicesoftwaretesting.pages.RegisterPage;
+import com.practicesoftwaretesting.pages.*;
+import com.practicesoftwaretesting.user.model.RegisterUserRequest;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
@@ -14,6 +12,7 @@ public class UserTest {
     Header header = new Header();
     LoginPage loginPage = new LoginPage();
     RegisterPage registerPage = new RegisterPage();
+    AccountPage accountPage = new AccountPage();
 
     @Test
     public void registerNewUserAndLogin() {
@@ -27,5 +26,32 @@ public class UserTest {
         registerPage.isLoaded()
                 .assertThat()
                 .hasCorrectInfo();
+
+        var user = getUser();
+        registerPage.registerNewUser(user);
+
+        loginPage.isLoaded()
+                .login(user.getEmail(), user.getPassword());
+
+        accountPage.isLoaded();
+        var fullUserName = user.getFirstName() + " " + user.getLastName();
+        header.assertThat()
+                .isSignedIn(fullUserName);
+    }
+
+    private static RegisterUserRequest getUser() {
+        return RegisterUserRequest.builder()
+                .firstName("George")
+                .lastName("Harrison")
+                .address("123 Main St")
+                .city("Liverpool")
+                .state("Merseyside")
+                .country("Ukraine")
+                .postcode("12345")
+                .phone("1234567890")
+                .dob("01/01/2000")
+                .email("georgeharrison@gmail.com")
+                .password("12Example#")
+                .build();
     }
 }
